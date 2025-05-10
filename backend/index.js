110c40e90a4b3efc7dbae2bcffad490a38acc885
bot-in-lab <mdalimran42@gmail.com>
2025-05-11 00:28:15 +0600

    Commit

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,10 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import connectDB from "./database/db.js";
 import userRoute from "./routes/user.route.js";
-// import courseRoute from "./routes/course.route.js";
-// import mediaRoute from "./routes/media.route.js";
+import courseRoute from "./routes/course.route.js";
+import mediaRoute from "./routes/media.route.js";
 // import purchaseRoute from "./routes/purchaseCourse.route.js";
-// import courseProgressRoute from "./routes/courseProgress.route.js";
+import courseProgressRoute from "./routes/courseProgress.route.js";
 
 dotenv.config({});
 
@@ -27,11 +27,11 @@ app.use(cors({
 }));
  
 // apis
-// app.use("/api/v1/media", mediaRoute);
+app.use("/api/v1/media", mediaRoute);
 app.use("/api/v1/user", userRoute);
-// app.use("/api/v1/course", courseRoute);
+app.use("/api/v1/course", courseRoute);
 // app.use("/api/v1/purchase", purchaseRoute);
-// app.use("/api/v1/progress", courseProgressRoute);
+app.use("/api/v1/progress", courseProgressRoute);
  
  
 app.listen(PORT, () => {
